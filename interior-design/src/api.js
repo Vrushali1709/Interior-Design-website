272f0129d2247f7355760a2d0b67f75d433eeb2a
Vrushali Panchal <vrushalipanchal31@gmail.com>
2026-06-25 12:30:50 +0530

    Link Update

--- interior-design/src/api.js
+++ interior-design/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://interior-design-backend-flag.onrender.com/",
+  baseURL: "https://interior-design-backend-flag.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
